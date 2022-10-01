ed58107e447aaefe62bb29961e9860ca1ebe0856
Anita Ding <anitading556@berkeley.edu>
2022-09-30 23:38:26 -0700

    Adding published websites

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var myWindow;
 // Functions to open a new tab to each of the 4 sites.
 function openHome() {
-	myWindow = window.open ('/Users/anitading556/Desktop/cs198-99/project1-anitading556-1/index.html')
+	myWindow = window.open ('https://fullstack-decal-fa22.github.io/project1-anitading556/')
 }
 
 function openAbout() {
-	myWindow = window.open ('/Users/anitading556/Desktop/cs198-99/project1-anitading556-1/about.html')
+	myWindow = window.open ('https://fullstack-decal-fa22.github.io/project1-anitading556/about.html')
 }
 
 function openPortfolio() {
-	myWindow = window.open ('/Users/anitading556/Desktop/cs198-99/project1-anitading556-1/portfolio.html')
+	myWindow = window.open ('https://fullstack-decal-fa22.github.io/project1-anitading556/portfolio.html')
 }
 
 function openContact() {
-	myWindow = window.open ('/Users/anitading556/Desktop/cs198-99/project1-anitading556-1/contact.html')
+	myWindow = window.open ('https://fullstack-decal-fa22.github.io/project1-anitading556/contact.html')
 }
