60d31df4f45ea12222cadf34a4f62e363ec39ccb
Anita Ding <anitading556@berkeley.edu>
2022-10-01 02:17:26 -0700

    testing alert usage

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function checkStart() {
 }
 // Functions to open a new tab to each of the 4 sites.
 function openHome() {
+	alert('Testing alert');
 	checkStart()
 	myWindow = window.open ('index.html');
 	start = false;
