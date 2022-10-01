60e7cc7abf01fdcc66f378760834b07693773ddb
Anita Ding <anitading556@berkeley.edu>
2022-10-01 01:22:01 -0700

    did it update?

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var myWindow;
 // Functions to open a new tab to each of the 4 sites.
 function openHome() {
-	myWindow = window.open ('https://fullstack-decal-fa22.github.io/project1-anitading556/')
+	myWindow = window.open ()
 }
 
 function openAbout() {
-	myWindow = window.open ('https://fullstack-decal-fa22.github.io/project1-anitading556/about.html')
+	myWindow = window.open ('about.html')
 }
 
 function openPortfolio() {
-	myWindow = window.open ('https://fullstack-decal-fa22.github.io/project1-anitading556/portfolio.html')
+	myWindow = window.open ('portfolio.html')
 }
 
 function openContact() {
-	myWindow = window.open ('https://fullstack-decal-fa22.github.io/project1-anitading556/contact.html')
+	myWindow = window.open ('contact.html')
 }
