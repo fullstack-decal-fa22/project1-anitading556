c46ddb595f82ba74aad15d4c2603e9a19e627245
Anita Ding <anitading556@berkeley.edu>
2022-10-01 02:15:18 -0700

    Added an alert for the initial tab so the user manually closes it for effect.

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 var myWindow;
+
+function checkStart() {
+	if (start == true) {
+		alert('Please close this tab. Click "back" on new tab for Home Page.');
+	}
+}
 // Functions to open a new tab to each of the 4 sites.
 function openHome() {
-	myWindow = window.open ('index.html')
+	checkStart()
+	myWindow = window.open ('index.html');
+	start = false;
 }
 
 function openAbout() {
-	myWindow = window.open ('about.html')
+	checkStart()
+	myWindow = window.open ('about.html');
+	start = false;
 }
 
 function openPortfolio() {
-	myWindow = window.open ('portfolio.html')
+	checkStart()
+	myWindow = window.open ('portfolio.html');
+	start = false;
 }
 
 function openContact() {
-	myWindow = window.open ('contact.html')
+	checkStart()
+	myWindow = window.open ('contact.html');
+	start = false;
 }
